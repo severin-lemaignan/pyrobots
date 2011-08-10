0cec0a8693dda9cf8cdab517ea78b90cd4523de6
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-08-10 14:30:26 +0200

    Added optional frame to novela_places.json + support in look_at

--- python/actions/look_at_genom.py
+++ python/actions/look_at_genom.py
@@ -3,12 +3,18 @@ from action import action, genom_request
 
 @action
 def look_at(place):
-    return look_at_xyz(place['x'], place['y'], place['z'])
+    try:
+	frame = place['frame']
+    catch KeyError:
+	frame = "/map"
+
+    return look_at_xyz(place['x'], place['y'], place['z'], frame)
 
 @action
 def look_at_xyz(x,y,z, frame = "/map"):
     """ Look at via pr2SoftMotion.
     """
+    print("Looking at " + str([x,y,z]) + " in " + frame)
     actions = [
         genom_request(	"pr2SoftMotion",
             "MoveHead",
