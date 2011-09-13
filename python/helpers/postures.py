03d504290ab2207234fd36b985c9dc8648a9e64e
Nicolas Courbet <ncourbet@laas.fr>
2011-09-13 16:19:15 +0200

    Added helpers/postures.py

---
+++ python/helpers/postures.py
@@ -0,0 +1,17 @@
+import json
+import logging; logger = logging.getLogger("novela." + __name__)
+logger.setLevel(logging.DEBUG)
+
+source = '../share/pr2_postures.json'
+
+_places = None
+
+def read():
+    global _places
+    if not _places:
+        logger.info("Reading symbolic places from " + source)
+        f = open(source,'r')
+        json_data=f.read()
+        _places = json.loads(json_data)
+    return _places
+
