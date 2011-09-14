92e76d84a86683ab5e13c113f1dce3ea6ca33716
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-09-14 11:50:36 +0200

    Rewrote gotoposutreraw to accept any configuration of the robot
    Any part of the PR2 can be modified (ARMS, TORSO, HEAD...).
    Also, renamed the function in setpose.
    
    Updated share/pr2_posture.json to the new format.

--- python/actions/configuration.py
+++ python/actions/configuration.py
@@ -1,4 +1,5 @@
 import random
+from exception import RobotError
 from action import action, genom_request
 from helpers import trajectory
 
@@ -42,25 +43,83 @@ def gotoposture(posture, obj, part = 'RARM', support = 'HRP2TABLE', use_cartesia
     return actions
 
 @action
-def gotopostureraw(posture, part = 'RARM', r_wrist_angle = 0.0, callback = None):
+def gotopostureraw(posture, part = None, callback = None):
     """
+    Set the PR2 joints in a given configuration.
+
     Like gotoposture except it does not plan (no collision avoidance)
+    
+    :param posture: a posture, ie, a dictionary built as follow:
+    <ul>
+     <li>Key <b>RARM</b>: array of 7 joint angles for the right arm, in radians,</li>
+     <li>Key <b>LARM</b>: array of 7 joint angles for the left arm, in radians,</li>
+     <li>Key <b>TORSO</b>: the height of the torso ([0-30]), in centimeters,</li>
+     <li>Key <b>HEAD</b>: array of pan and tilt values, in radians,</li>
+    :param part: (optional) force only a certain part of the PR2 to execute to posture.
+    Allowed values: 'RARM', 'LARM', 'ARMS', 'PR2', 'PR2SYN', 'TORSO', 'PR2NOHEAD', 'HEAD'
+    :param callback: (optional) If given, the action is non-blocking, and the callback is
+    invoked at the activity completion.
     """
-    if part not in ['RARM']:
+    if part not in ['RARM', 'LARM', 'ARMS', 'PR2', 'PR2SYN', 'TORSO', 'PR2NOHEAD', 'HEAD']:
 	print("'Go to posture raw' for part " + part + " is not implemented.")
 
     print (posture)
-    q1, q2, q3, q4, q5, q6, q7 = posture
+
+    forced_part = part
+
+    try:
+        rq1, rq2, rq3, rq4, rq5, rq6, rq7 = posture['RARM']
+        if not forced_part:
+            part = 'RARM'
+    except KeyError:
+        rq1, rq2, rq3, rq4, rq5, rq6, rq7 = [0.0] * 7
+
+    try:
+        lq1, lq2, lq3, lq4, lq5, lq6, lq7 = posture['LARM']
+        if not forced_part:
+            if part = 'RARM':
+                part = 'ARMS'
+            else:
+                part = 'LARM'
+    except KeyError:
+        lq1, lq2, lq3, lq4, lq5, lq6, lq7 = [0.0] * 7
+
+    try:
+        torso = posture['TORSO']
+        if not forced_part:
+            if part = 'ARMS':
+                part = 'PR2NOHEAD'
+            elif not part:
+                part = 'TORSO'
+            else:
+                raise RobotError("Can not move only one arm and the torso in one call. Please call gotopostureraw twice.")
+    except KeyError:
+        torso = 0.0
+
+    try:
+        pan, tilt = posture['HEAD']
+        if not forced_part:
+            if part = 'PR2NOHEAD':
+                part = 'PR2'
+            elif not part:
+                part = 'HEAD'
+            else:
+                raise RobotError("Can not move only one arm or the torso and the head in one call. Please call gotopostureraw twice.")
+    except KeyError:
+        torso = 0.0
+
+
     actions = [
-	genom_request("pr2SoftMotion", "GotoQ",
-    		[part,
-    		 0,
-    		 0.2, 0.0, 0.6, 0.0,
-    		 q1, q2, q3, q4, q5, q6, q7, r_wrist_angle, 0.0, # Right arm
-    		 0.73, 0.86, 0.06, -2.09, 2.42, -1.29, -2.95, 0.0, 0.0], # Left arm (ignored)
-		wait_for_completion = False if callback else True,
-		callback = callback
-	)	
+	genom_request(
+            "pr2SoftMotion", 
+            "GotoQ",
+    		[    forced_part if forced_part else part,
+        		 0,
+        		 torso, pan, tilt, 0.0,
+    	    	 rq1, rq2, rq3, rq4, rq5, rq6, rq7, 0.0, 0.0, # Right arm
+    		     lq1, lq2, lq3, lq4, lq5, lq6, lq7, 0.0, 0.0], # Left arm
+       		wait_for_completion = False if callback else True,
+	    	callback = callback)
     ]
     return actions
 
