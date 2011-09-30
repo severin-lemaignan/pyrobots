7bb567e3037f664c82b60ffa53d297a85557543c
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-09-30 10:52:30 +0200

    [novela] Updated version of the main Novela play script

--- python/scenarii/novela.py
+++ python/scenarii/novela.py
@@ -23,6 +23,10 @@ from actions.primitive_nav import *
 from actions.look_at import *
 from actions.manipulation import *
 from actions.configuration import *
+from actions.games import *
+from actions.administration import *
+
+SCE_PATH = "/u/slemaign/openrobots/share/move3d/assets/novela/SCENARIO/"
 
 def getpr2():
 	return ActionPerformer(['pr2c2', 'pr2c1'], 9472)
@@ -31,7 +35,7 @@ def printer(e):
 	print("End of lookat!! " + str(e))
 
 #TODO: Fix 'carry'
-
+#TODO: pour gagner du temps, prise d'objet dans l'embrasure
 def gotoniche():
     #TODO: track niche 
     #robot.execute(track, p["NICHE"])
@@ -50,13 +54,18 @@ def gotohq():
 
 def idling():
     #TODO: trigger it automatically
-    pass
+    robot.execute(restpose)
+    robot.execute(manipose)
+    robot.execute(glance_to, p["TABLE"])
+    robot.execute(glance_to, p["HQ"])
+    robot.execute(look_at, p["AUDIENCE"])
 
 def s2p1():
+    #TODO: ouverture rideau, passage technos
     robot.execute(goto, p["AUDIENCE_WATCHING"])
     robot.execute(look_at, p["AUDIENCE"])
     robot.execute(sweep_look)
-    gotoniche()
+    robot.execute(goto, p["JARDIN_ENTER_IN"])
 
 def s2p2():
     #TODO: track Xavier
@@ -67,16 +76,17 @@ def s2p2():
 def s2p3():
     #TODO: track table 
     #TODO: en fait, Xavier lui montre les tags
-    robot.execute(look_at, p["TABLE"])
-    robot.execute(goto, p["TABLE"])
-    robot.execute(look_at, p["TABLE"])
-    robot.execute(sweep_look)
-    #TODO: track hanger 
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(goto, p["HANGER_WATCHING"])
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(sweep_look)
-    gotoniche()
+    robot.execute(look_at, p["TABLE_CENTER"])
+    robot.execute(goto, p["TABLE_CORNER"])
+    robot.execute(look_at, p["HANGER"])
+    robot.execute(sweep_look, 10, 0.1)
+    robot.execute(look_at, p["TABLE_CENTER"])
+    robot.execute(sweep_look, 80, 0.1)
+    robot.execute(look_at, p["NICHE"])
+    robot.execute(goto, p["NICHE_WATCHING"])
+    robot.execute(look_at, p["NICHE"])
+    robot.execute(sweep_look, 80, 0.1)
+    robot.execute(goto, p["NICHE"])
 
     #TODO idle in niche
 
@@ -106,20 +116,21 @@ def s3p3():
     """
     Ventilo
     """
+    poses = postures.read()
     robot.execute(look_at, p["HQ"])
     robot.execute(goto, p["HQ"])
     robot.execute(basicgrab)
     robot.execute(tuckedpose, nop)
     robot.execute(goto, p["JARDIN_ENTER_IN"])
-    robot.execute(manipose, nop)
-    # TODO: position angle table pour ventilo
-    robot.execute(goto, p["TABLE"])
-    robot.execute(basicgive)
+    robot.execute(setpose, poses["GIVE"], nop)
+    robot.execute(goto, p["TABLE_CORNER"])
 
 def s3p31():
     """Geste desespoir milieu scene
     """
-    robot.execute(goto, p["CENTER"])
+    robot.execute(release_gripper)
+    #TODO: geste desespoir
+    robot.execute(goto, p["CENTER_FACE"])
 
 def s3p32():
     """Retour niche
@@ -139,20 +150,16 @@ def s3p5():
     Exploration de nuit 
     """
     robot.execute(goto, p["JARDIN_ENTER_IN"])
+    #TODO:regarder le sol devant proscenium
     robot.execute(look_at, p["TABLE"])
     robot.execute(goto, p["TABLE"])
     robot.execute(look_at, p["TABLE"])
     robot.execute(sweep_look)
 
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(goto, p["HANGER_WATCHING"])
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(sweep_look)
-
     robot.execute(look_at, p["HQ"])
     robot.execute(tuckedpose, nop)
     robot.execute(goto, p["HQ"])
-    # TODO: scene SPARK bordel
+    robot.execute(setup_scenario, [SCE_PATH + "bazar.sce"])
 
 def s3p6():
     """
@@ -160,35 +167,19 @@ def s3p6():
     """
     gotoniche()
 
-def s3p7():
-    """
-    Re-exploration autour de Xavier qui remet les tags en place 
-    """
-    robot.execute(look_at, p["TABLE"])
-    robot.execute(goto, p["TABLE"])
-    robot.execute(look_at, p["TABLE"])
-    robot.execute(sweep_look)
-
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(goto, p["HANGER_WATCHING"])
-    robot.execute(look_at, p["HANGER_WATCHING"])
-    robot.execute(sweep_look)
-
-    gotoniche()
-
 def s4p1():
     """
     Corbeille
     """
+    robot.execute(setup_scenario, [SCE_PATH + "clean.sce"])
     robot.execute(look_at, p["HQ"])
-    robot.execute(goto, p["HQ"])
+    robot.execute(goto, p["JARDIN_ENTER_OUT"])
+    robot.execute(look_at, p["TABLE"], nop)
     robot.execute(basicgrab)
     #TODO: adjust basket game posture
     #robot.execute(tuckedpose, nop)
     robot.execute(goto, p["JARDIN_ENTER_IN"])
     #robot.execute(manipose, nop)
-    #TODO: adjust basket game position
-    robot.execute(goto, p["CENTER"])
 
 def basket_game(): 
 	j = 0
@@ -236,6 +227,20 @@ def basket_game():
 		time.sleep(1.0)
 		x_prec, y_prec = x, y
 		j += 1
+
+def s5p1():
+    robot.execute(goto, p["AUDIENCE_WATCHING"])
+
+def s5p2():
+    poses = postures.read()
+    robot.execute(gym)
+    # PR2 se met en position menacante
+    robot.execute(goto, p["MONSTER_POSE"], nop)
+    robot.execute(setpose, poses["ARMS_UP"])
+
+def s5p3():
+    robot.execute(tuckedpose, nop)
+    robot.execute(goto, p["CENTER_FACE"])
  
 if __name__=="__main__":
 
@@ -248,47 +253,58 @@ if __name__=="__main__":
 #      robot.execute(tuckedpose)
 #      robot.execute(goto, p["HQ"])
 #      robot.execute(look_at, p["LOOK_NOWHERE"])
-#      raw_input("Press enter when robot in place...")
-#  
-#      s2p1()
-#      raw_input("Press enter when Xavier is recognized...")
-#      s2p2()
-#      raw_input("Press enter when Xavier in place...")
-#      s2p3()
+#      print("[[[[[[[[[[[[[ END OF INIT ]]]]]]]]]]]]]")
+      raw_input("Press enter when robot in place...")
+  
+      s2p1()
+      raw_input("Press enter when Xavier is recognized...")
+      s2p2()
+      raw_input("Press enter when Xavier in place...")
+      s2p3()
+#      idling()
 #      raw_input("Press enter when Xavier is thirsty...")
-#      #TODO idling
 #      s2p4()
 #      print("[[[[[[[[[[[[[ END OF SCENE 2 ]]]]]]]]]]]]]")
 #      raw_input("Press enter when robot need to track chair tag...")
 #      #TODO idling
 #      s3p1()
 #      #TODO idling
+#      idling()
 #
-#      raw_input("Press enter when Xavier is tooo hot...")
+#      raw_input("Press enter when PR2 need to look at Xavier...")
 #
 #      s3p2()
 #      raw_input("Press enter when Xavier is tooo hot...")
 #      s3p3()
-#      raw_input("Press enter when PR2 must be desesperate...")
+#      raw_input("Press enter when PR2 must release ventilo and be desesperate...")
 #      s3p31()
 #      raw_input("Press enter when PR2 must go back to niche...")
 #      s3p32()
 #      raw_input("Press enter when PR2 must go back the technos...")
 #      s3p4()
-#
+
 #      raw_input("Press enter when PR2 has the frontale...")
 #      s3p5()
 #      raw_input("Press enter to bring PR2 to the niche...")
 #      s3p6()
-#      
+      
 #      print("[[[[[[[[[[[[[ END OF SCENE 3 ]]]]]]]]]]]]]")
 #      raw_input("Press enter to get the basket...")
 #      s4p1()
-#      raw_input("Press enter to bring PR2 to play the basket game...")
-      basket_game()
-      gotohq()
+#      raw_input("Press enter when finished with basket game...")
+#      #raw_input("Press enter to bring PR2 to play the basket game...")
+#      #basket_game()
+#      gotohq()
+#      print("[[[[[[[[[[[[[ END OF SCENE 4 ]]]]]]]]]]]]]")
       
-      print("[[[[[[[[[[[[[ END OF SCENE 4 ]]]]]]]]]]]]]")
+#      raw_input("Press enter to send PR2 to gym...")
+#      s5p1()
+#      raw_input("Press enter to start gym (start 38 sec after music start)...")
+#      s5p2()
+#      raw_input("Press enter to go back to center...")
+#      s5p3()
+
+#      print("[[[[[[[[[[[[[ END OF SCENE 5 ]]]]]]]]]]]]]")
     except Exception as e:
       print(e)
     finally:
