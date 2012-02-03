7f0c651419829d9052a3b1360a67a0309efb1ff3
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-02-03 11:01:45 +0100

    Converted the project into a real python project
    - Moved relevant code to src/
    - added a setup.py file
    - added AUTHORS, LICENSE, README files
    - renamed remaining old files folder in old/ (TODO: clean that)

---
+++ setup.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+
+setup(name='pyRobots',
+      version='0.1',
+      license='ISC',
+      description='A collection of Python scripts to interface with PR2 ROS and GenoM modules',
+      author='OpenRobots team',
+      author_email='openrobots@laas.fr',
+      package_dir = {'': 'src'},
+      packages=['robots', 'robots.actions', 'robots.helpers'],
+      #scripts=['scripts/robots_test'], # Do not exist yet :-(
+      data_files=[('share/pyrobots', ['share/' + f for f in ['pr2_postures.json', 'novela_places.json']]),
+                  ('share/doc/pyrobots', ['AUTHORS', 'LICENSE', 'README'])]
+      )
