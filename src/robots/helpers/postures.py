5ad17fe9612cbe066f715dd2b6cdac8c2999db6a
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-02-23 18:39:54 +0100

    Extended helpers/postures to have several sources and explicitely load json files

--- src/robots/helpers/postures.py
+++ src/robots/helpers/postures.py
@@ -6,17 +6,29 @@ logger.setLevel(logging.DEBUG)
 #hack that tries to find out the current prefix and then the data directory
 DATA_DIR = os.path.abspath(__file__).split('lib')[0] + '/share/pyrobots/'
 
-
-source = DATA_DIR + 'pr2_postures.json'
+#TODO: make something more generic!!
+sources = [DATA_DIR + 'pr2_postures.json']
+sources += [DATA_DIR + 'poses_hri_expe_feb2012.json']
 
 _places = None
 
 def read():
     global _places
     if not _places:
-        logger.info("Reading symbolic places from " + source)
-        f = open(source,'r')
-        json_data=f.read()
-        _places = json.loads(json_data)
+	_places = {}
+	for source in sources:
+        	logger.info("Reading symbolic places from " + source)
+		with open(source,'r') as f:
+			json_data=f.read()
+		_places.update(json.loads(json_data))
+    return _places
+
+def load(source):
+    global _places
+    _places = {}
+    logger.info("Reading symbolic places from " + source)
+    with open(source,'r') as f:
+	json_data=f.read()
+	_places = json.loads(json_data)
     return _places
 
