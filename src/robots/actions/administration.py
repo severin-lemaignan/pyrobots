33190b54f1742069c98b25f96e3ec609fb8c8140
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-03-13 15:44:34 +0100

    [actions/admin] Completed the initialization code
    viman, niut, pr2Softmotion and spark are now initialized at startup.
    
    Re-entrance is ~ok, except for niut that will crash if Init is called
    more than 4 times in a row.
    
    Also issue with spark::LoadP3D that crashes when called twice.

--- src/robots/actions/administration.py
+++ src/robots/actions/administration.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import logging; logger = logging.getLogger("robot." + __name__)
 logger.setLevel(logging.DEBUG)
 
@@ -10,6 +11,7 @@ from robots.action import *
 from robots.action import wait as basewait
 
 from robots.actions.manipulation import configure_grippers
+from robots.actions.configuration import settorso
 
 @action
 def setup_scenario(robot, scenario):
@@ -53,29 +55,67 @@ def wait(robot, seconds):
 @tested("22/02/2012")
 @action
 def init(robot, \
+        viman = True, \
+        niut = True, \
         pr2SoftMotion = True, \
-        spark = False,
+        spark = True,
         p3d = ""):
     """ Initialize modules in correct order.
+
+        Modules whose initialization is reentrant (ie, init code can be safely called
+        twice) are enabled by default.
     """
 
+    actions = []
+
+    if viman:
+        actions += [genom_request("viman", "Init", ["VIMAN_INPUT_VIAM", # Input stereo stream
+                                                   "VIMAN_ENABLE", # use stereo
+                                                   "VIMAN_DISABLE", # use color
+                                                   os.environ["ROBOTPKG_BASE"] + "/share/viman", # object config path
+                                                   "vimanBridgeImage", # input poster
+                                                   "CAMERA_TOP_LEFT", # left camera name
+                                                   "CAMERA_TOP_RIGHT", # right camera name
+                                                   1, # stereo ratio baseline
+                                                   "VIMAN_ENABLE", # display status
+                                                   0]), # unused
+                    genom_request("viman", "ActivateTracking", callback = nop)]
+
+    if niut:
+        actions += [genom_request("niut", "Init")]
+
     if pr2SoftMotion:
-        actions = configure_grippers(robot)
+        actions += configure_grippers(robot)
         actions += [genom_request("pr2SoftMotion", "Init")]
+        actions += settorso(robot, 10)
+
+    # SPARK initialization
     if spark:
         if not os.path.exists(p3d):
-            raise RobotError("P3D file not found!")
-        actions += [genom_request("spark", "LoadP3d", [p3d, 1, 0]),
-                    genom_request("spark", "UpdateInterface", callback = nop),
-                    genom_request("spark", "SetInterfaceParams", [1200, 900, 0, 1, 1, 0, 0, 0]), #Scale up the SPARK window·
-                    genom_request("spark", "SetInterfaceAgentParams", ["ACHILE_HUMAN1", 1, 1, 0]), # Display visibility and pointing cones.
-                    genom_request("spark", "ChangeCameraPos", [4, -4, 1, 5, -0.7, 0.7]), #Move the camera
-                    genom_request("spark", "ReadRobot", ["lwrCurrentPoseArmRight", "lwrSAHand" , "pomPos", "pomPlatineFramePos", "."], callback = nop), # LWR poster, sahand poster, POM poster, Platine poster, sparkyarp robot config
+            logger.error("P3D file not found! Skipping the loading of the environment")
+        else:
+            actions += [genom_request("spark", "LoadP3d", [p3d, 1, 0])]
+
+        actions += [genom_request("spark", "UpdateInterface", callback = nop),
+                    genom_request("spark", "SetInterfaceParams", [1200, 900, #Scale up the SPARK window·
+                                                                  "GEN_FALSE", # Save interface
+                                                                  "GEN_TRUE", # Enable floors
+                                                                  "GEN_FALSE", # Enable walls
+                                                                  "GEN_FALSE", # Enable shadows
+                                                                  "GEN_FALSE", # Enables tiles
+                                                                  "GEN_FALSE"]), # Antialiasing
+                    genom_request("spark", "SetInterfaceAgentParams", ["HERAKLES_HUMAN1", 1, 1, 0]), # Display visibility and pointing cones.
+                    genom_request("spark", "ChangeCameraPos", [6, -2, 1, 5, -1, 0.5]), #Move the camera
+                    genom_request("spark", "SetKinectFixedFrame", ["GEN_TRUE", 3.0, 0.0, 2.51, 0, 0.45, -0.78]), #Place the fixed Kinect above Rachid's door
+                    genom_request("spark", "ReadRobot", ["ignored", "ignored" , "ignored", "ignored", "ignored", "pr2Pose", "pr2JointState", "pr2JointMap"], callback = nop), # LWR poster, sahand poster, POM poster, Platine poster, sparkyarp robot config
                     genom_request("spark", "ReadHumans", ['USE_NIUT', 0 , 'niutHuman'], callback = nop),
-                    genom_request("spark", "ReadObjects", ['USE_VIMAN', 0 , 'vimanObjectPose'], callback = nop),
-                    genom_request("spark", "SetKBAddress", ["localhost", 6969]),
-                    genom_request("spark", "ComputeFacts", callback = nop)
-                    ]
+                    genom_request("spark", "ReadObjects", ['USE_VIMAN', 0 , 'vimanObjectPose'], callback = nop)]
+
+        if robot.knowledge: # The robot has been created with an knowledge base, let's use it
+            logger.info("I'm connected to a knowledge base. Let's start filling it from perception!")
+            actions += [genom_request("spark", "SetKBAddress", [robot.knowledge.host, robot.knowledge.port]),
+                        genom_request("spark", "ComputeFacts", callback = nop)
+                        ]
 
     return actions
 
