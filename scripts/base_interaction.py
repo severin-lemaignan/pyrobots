eed1903743b444652926e51ffc9d3dcf447fd2b6
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-04-12 10:16:01 +0200

    Initial support of emotions in the control.
    Only prints out the current emotional state of the robot for now.

--- scripts/base_interaction.py
+++ scripts/base_interaction.py
@@ -28,22 +28,33 @@ def ondesires(e):
     for d in e:
         incoming_desires.put(d)
 
+def onemotion(e):
+    logger.info("New emotional state:" + str(e))
+
 with robots.PR2(knowledge = pyoro.Oro(), init = False) as pr2:
 
     if not "--noinit" in sys.argv:
         pr2.init(p3d = "/u/slemaign/openrobots/share/move3d/assets/ADREAM/ADREAM.p3d",
                  sce = "/u/slemaign/openrobots/share/move3d/assets/ADREAM/SCENARIOS/ADREAM-salon-only.sce")
         pr2.setenvironment()
+
+    # Subscribe to new human orders
     pr2.knowledge.subscribe([human + " desires ?d"], ondesires)
 
+    # Subscribe to changes of emotional state
+    pr2.knowledge.subscribe(["myself experiences ?s"], onemotion)
     try:
         logger.info("Waiting for desires...")
         while True:
             sit = incoming_desires.get()
 
             if sit:
-                desire = desires.desire_factory(sit, pr2)
-                desire.perform()
+                try:
+                    desire = desires.desire_factory(sit, pr2)
+                    desire.perform()
+                except NotExistingDesireTypeError as e:
+                    logger.error(e)
+                    logger.info("Skipping this desire.")
             time.sleep(0.1)
     except KeyboardInterrupt:
         pass
