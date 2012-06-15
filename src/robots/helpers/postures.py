20dde93e0303e29878629f192afc4eddfaa04a4b
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-06-15 18:58:12 +0200

    Identation

--- src/robots/helpers/postures.py
+++ src/robots/helpers/postures.py
@@ -8,19 +8,18 @@ DATA_DIR = os.path.abspath(__file__).split('lib')[0] + 'share/pyrobots/'
 
 #TODO: make something more generic!!
 sources = [DATA_DIR + 'pr2_postures.json']
-sources += [DATA_DIR + 'poses_hri_expe_feb2012.json']
 
 _places = None
 
 def read():
     global _places
     if not _places:
-	_places = {}
-	for source in sources:
-        	logger.info("Reading symbolic places from " + source)
-		with open(source,'r') as f:
-			json_data=f.read()
-		_places.update(json.loads(json_data))
+        _places = {}
+        for source in sources:
+            logger.info("Reading symbolic places from " + source)
+            with open(source,'r') as f:
+                json_data=f.read()
+            _places.update(json.loads(json_data))
     return _places
 
 def load(source):
@@ -28,7 +27,7 @@ def load(source):
     _places = {}
     logger.info("Reading symbolic places from " + source)
     with open(source,'r') as f:
-	json_data=f.read()
-	_places = json.loads(json_data)
+        json_data=f.read()
+        _places = json.loads(json_data)
     return _places
 
