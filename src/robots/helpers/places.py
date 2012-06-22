397d982d55e4fd5bf4600b8d24e686927181ce68
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-06-22 18:00:45 +0200

    Update helpers/places.json to read appart_places.json

--- src/robots/helpers/places.py
+++ src/robots/helpers/places.py
@@ -1,16 +1,16 @@
 import os
 import json
-import logging; logger = logging.getLogger("novela." + __name__)
+import logging; logger = logging.getLogger("robot." + __name__)
 logger.setLevel(logging.DEBUG)
 
 #hack that tries to find out the current prefix and then the data directory
 DATA_DIR = os.path.abspath(__file__).split('lib')[0] + '/share/pyrobots/'
 
-source = DATA_DIR + 'novela_places.json'
+source = DATA_DIR + 'appart_places.json'
 
 _places = None
 
-def read():
+def places():
     global _places
     if not _places:
         logger.info("Reading symbolic places from " + source)
