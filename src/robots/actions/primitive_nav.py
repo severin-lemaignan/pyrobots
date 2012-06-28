229f5fd00e1f60c442ef06971445f3c70cd4d589
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-06-28 12:23:22 +0200

    Rewritten primitive_nav.translate() to use ROS. Added a primitive_nav.dock action
    dock() tries to move closer to an obstacle in front of it (using
    the base laser scan)

--- src/robots/actions/primitive_nav.py
+++ src/robots/actions/primitive_nav.py
@@ -1,20 +1,76 @@
-from robots.action import action, broken
+import logging; logger = logging.getLogger("robot." + __name__)
+
+from robots.action import *
 from configuration import setpose
 
-@broken
+def ros_translate(robot, x, y = 0.0, speed = 0.1):
+    import math
+    import roslib; roslib.load_manifest('pyrobots_actionlib')
+    import rospy
+    from geometry_msgs.msg import Twist
+
+    rate = rospy.Rate(10.0)
+
+    pub = rospy.Publisher('/base_controller/command', Twist)
+
+    base_cmd = Twist()
+
+    distance = math.sqrt(x*x + y*y)
+
+    base_cmd.linear.x = (x/distance) * speed
+    base_cmd.linear.y = (y/distance) * speed
+
+    init_pose = robot.poses.myself()
+
+    done = False
+    while not done:
+        pub.publish(base_cmd)
+        rate.sleep()
+
+        dist_moved = robot.poses.distance(init_pose, robot.poses.myself())
+        logger.info("Moved " + str(dist_moved) + "m")
+
+        if (dist_moved > distance):
+            done = True
+
+    return done
+
+
+@tested("26/06/2012")
 @action
-def translate(robot, x, y = 0):
+def translate(robot, x, y = 0, speed = 0.1):
     """Move the robot base in XY, relatively to its current position.
     
     Be careful: this low-level action does NO obstacle avoidance.
+
+    ROS version is a Python translation of 
+    http://www.ros.org/wiki/pr2_controllers/Tutorials/Using%20the%20base%20controller%20with%20odometry%20and%20transform%20information
 
     Currently broken: the GotoQ request does not behave as expected.
 
     :param x: X (forward/backward) displacement, in meter
     :param y: (default: 0) Y (left/right) displacement, in meter
+    :param speed: speed, in m/s. Default: 0.1
     """
-    
-    return setpose(robot, {'BASE':[x,y,0,0,0,0]}, relative = True)
+    #gotoQ version, broken
+    #return setpose(robot, {'BASE':[x,y,0,0,0,0]}, relative = True)
+    return [python_request(ros_translate, [x, y, speed])]
+
+@tested("27/06/2012")
+@action
+def dock(robot, distance = 0.2, max_distance = 1):
+
+    def execute_dock(robot):
+        if robot.state.distance2obstacle() > max_distance:
+            return False
+
+        if robot.state.distance2obstacle() < distance:
+            return True
+
+
+        return ros_translate(robot, robot.state.distance2obstacle() - distance)
+
+    return [python_request(execute_dock, [])]
 
 
 @broken
