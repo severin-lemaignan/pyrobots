2b69e7159441d81da69f57f85bf55e7f3a1a9f11
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2013-10-24 16:51:33 +0200

    Updated 'scripts/base_interaction.py' to be a real app
    While here:
    - replaced pyoro by the generic pykb
    - added a couple of cmd line arguments to set the debug level
     and allow to run with a dummy middleware

--- scripts/base_interaction.py
+++ scripts/base_interaction.py
@@ -1,61 +1,86 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import logging
-logger = logging.getLogger("robot")
-logger.setLevel(logging.DEBUG)
-
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)-15s %(name)s: %(levelname)s - %(message)s')
-console.setFormatter(formatter)
-
-logger.addHandler(console)
-
 import sys
 import time
 import Queue as queue
 
-import pyoro
+import kb
 import robots
-from robots import desires
+from robots.behaviours import desires, interrogation
 
-human = "HERAKLES_HUMAN1"
+human = "HUMAN1"
 
 incoming_desires = queue.Queue()
 
 def ondesires(e):
-    logger.info("Incomig desires:" + str(e))
+    logger.info("Incoming desires:" + str(e))
     for d in e:
         incoming_desires.put(d)
 
 def onemotion(e):
     logger.info("New emotional state:" + str(e))
 
-with robots.PR2(knowledge = pyoro.Oro(), init = False) as pr2:
+def start_interaction(dummy = False):
+    with robots.Robot(knowledge = kb.KB(), dummy = dummy) as robot:
 
-    if not "--noinit" in sys.argv:
-        pr2.init(p3d = "/u/slemaign/openrobots/share/move3d/assets/ADREAM/ADREAM.p3d",
-                 sce = "/u/slemaign/openrobots/share/move3d/assets/ADREAM/SCENARIOS/ADREAM-salon-only.sce")
-        pr2.setenvironment()
+        # Subscribe to new human orders
+        robot.knowledge.subscribe([human + " desires ?d"], ondesires)
 
-    # Subscribe to new human orders
-    pr2.knowledge.subscribe([human + " desires ?d"], ondesires)
+        # Subscribe to changes of emotional state
+        robot.knowledge.subscribe(["myself experiences ?s"], onemotion)
+        try:
+            logger.info("Waiting for desires...")
+            while True:
+                sit = incoming_desires.get()
 
-    # Subscribe to changes of emotional state
-    pr2.knowledge.subscribe(["myself experiences ?s"], onemotion)
-    try:
-        logger.info("Waiting for desires...")
-        while True:
-            sit = incoming_desires.get()
+                if sit:
+                    try:
+                        desire = desires.desire_factory(sit, robot)
+                        desire.perform()
+                    except desires.NotExistingDesireTypeError as e:
+                        logger.error(e)
+                        logger.info("Skipping this desire.")
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
 
-            if sit:
-                try:
-                    desire = desires.desire_factory(sit, pr2)
-                    desire.perform()
-                except desires.NotExistingDesireTypeError as e:
-                    logger.error(e)
-                    logger.info("Skipping this desire.")
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        pass
 
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='pyrobots base application.')
+    parser.add_argument('-d', '--debug', action='store_true',
+                                help='enables verbose output')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                                help='be quiet (only errors are reported)')
+    parser.add_argument('--dummy', action='store_true',
+                                help='use a dummy middleware (outputs all actions to stdout)')
+    args = parser.parse_args()
+
+
+
+    logger = logging.getLogger("robot")
+    kblogger = logging.getLogger("kb")
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        kblogger.setLevel(logging.DEBUG)
+    elif args.quiet:
+        logger.setLevel(logging.ERROR)
+        kblogger.setLevel(logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+        kblogger.setLevel(logging.INFO)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)-15s %(name)s: %(levelname)s - %(message)s')
+    console.setFormatter(formatter)
+
+    logger.addHandler(console)
+    kblogger.addHandler(console)
+
+    start_interaction(args.dummy)
