b1aef1973901b9ac1521ba431fd236d117001fa6
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-05-16 15:46:40 +0200

    Added a little helpers to print logger hierarchy

--- src/robots/helpers/helpers.py
+++ src/robots/helpers/helpers.py
@@ -28,3 +28,69 @@ class valuefilter:
             self.dirty = False
 
         return self.lastval
+
+
+
+## Taken from http://code.activestate.com/recipes/578389-print-logger-internals/
+import logging
+import logging.handlers
+
+
+def enable_logger_print():
+    def _logger_str(self):
+        s = ''
+        if self.parent != None:
+            s =  _logger_str(self.parent)
+        s += """\n%s\n""" % (self.name)
+        for name,value in self.__dict__.items():
+            if name == 'parent': value = value and value.name or 'None'
+            if name == 'level': value = '%s (%s)' %(value,logging.getLevelName(value))
+            s += '    %s = %s\n' % (name,value)
+        return s
+
+    def _handler_repr(handlername):
+        def __repr__(self):
+            s = '\n\t%s\n' % (handlername)
+            for name,value in self.__dict__.items():
+                if name == 'level': value = '%s (%s)' %(value,logging.getLevelName(value))
+                s += '\t    %s = %s\n' % (name,value)
+            s += '\t'
+            return s
+        return __repr__
+
+    def _formatter_repr(self):
+        s = ""
+        for name,value in self.__dict__.items():
+            s += '\n\t\t%s = %s' % (name,value)
+        return s
+
+    def _manager_repr(self):
+        s = ""
+        for name,value in self.__dict__.items():
+            if name == 'root': value = value.name
+            if name == 'loggerDict': value = _logger_list(); name='loggerDict keys'
+            s += '\n        %s = %s' % (name,value)
+        return s
+
+    def _filter_repr(self):
+        return self.name
+
+    def _logger_list():
+        return sorted([name for name in logging.Logger.manager.loggerDict])
+
+    for name in dir():
+        if name.endswith('Handler'):
+            logging.__dict__[name].__repr__ = _handler_repr(name)
+
+    for name in dir(logging):
+        if name.endswith('Handler'):
+            logging.__dict__[name].__repr__ = _handler_repr(name)
+
+    for name in dir(logging.handlers):
+        if name.endswith('Handler'):
+            logging.handlers.__dict__[name].__repr__ = _handler_repr(name)
+
+    logging.Logger.__str__ = _logger_str
+    logging.Formatter.__repr__ = _formatter_repr
+    logging.Filter.__repr__ = _filter_repr
+    logging.Manager.__repr__ = _manager_repr
