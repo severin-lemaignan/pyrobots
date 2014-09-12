severin-lemaignan/pyrobots

commit 0745cb3cdd36744f5eb3a813cd060fdf084278cb

Support for predicate to create events
Those predicates (function or lambda returning a boolean) *must* take a single argument, the robot instance.

src/robots/events.py
@@ -1,4 +1,5 @@
 import logging; logger = logging.getLogger("robots.events")
+import time
 import weakref
 
 import threading # for current_thread()
@@ -99,8 +100,8 @@ class EventMonitor:
 
         self.robot = robot
 
-        if var not in robot.state:
-            raise Exception("%s is not part of the robot state" % var)
+        if var not in robot.state and not callable(var):
+            raise Exception("%s is neither a member of the robot's state or a predicate" % var)
 
         self.var = var
 
@@ -118,26 +119,27 @@ class EventMonitor:
             self.start_dec_value = self.robot.state[self.var] 
             self.last_value = self.robot.state[self.var] 
 
-        if value is not None:
-            self.mode = EventMonitor.VALUE
-            self.target = value
-        elif becomes is not None:
-            self.mode = EventMonitor.BECOMES
-            self.target = becomes
-        elif above is not None:
-            self.mode = EventMonitor.ABOVE
-            self.target = above
-        elif below is not None:
-            self.mode = EventMonitor.BELOW
-            self.target = below
-        elif increase is not None:
-            self.mode = EventMonitor.INCREASE
-            self.target = increase
-        elif decrease is not None:
-            self.mode = EventMonitor.DECREASE
-            self.target = decrease
-        else:
-            raise Exception("Event created without condition!")
+        if not callable(self.var):
+            if value is not None:
+                self.mode = EventMonitor.VALUE
+                self.target = value
+            elif becomes is not None:
+                self.mode = EventMonitor.BECOMES
+                self.target = becomes
+            elif above is not None:
+                self.mode = EventMonitor.ABOVE
+                self.target = above
+            elif below is not None:
+                self.mode = EventMonitor.BELOW
+                self.target = below
+            elif increase is not None:
+                self.mode = EventMonitor.INCREASE
+                self.target = increase
+            elif decrease is not None:
+                self.mode = EventMonitor.DECREASE
+                self.target = decrease
+            else:
+                raise Exception("Event created without condition!")
 
         logger.info("Added new event monitor: %s" % self)
 
@@ -224,17 +226,25 @@ class EventMonitor:
     def _wait_for_condition(self):
 
         if not self.robot.dummy:
-            if self.var not in self.robot.state:
-                # value not yet read from the robot.
-                logger.warning("Waiting for %s to be published by the robot..." % self.var)
-                while not self.var in self.robot.state:
-                    self.robot.wait_for_state_update(2)
 
-            while not self._check_condition(self.robot.state[self.var]):
-                if not self.monitoring:
-                    logger.info("<%s> not monitored anymore" % str(self))
-                    return False
-                self.robot.wait_for_state_update(ACTIVE_SLEEP_RESOLUTION)
+            # predicate-based event
+            if callable(self.var):
+                while not self.var(self.robot):
+                    time.sleep(ACTIVE_SLEEP_RESOLUTION)
+
+            # state-based event
+            else:
+                if self.var not in self.robot.state:
+                    # value not yet read from the robot.
+                    logger.warning("Waiting for %s to be published by the robot..." % self.var)
+                    while not self.var in self.robot.state:
+                        self.robot.wait_for_state_update(2)
+
+                while not self._check_condition(self.robot.state[self.var]):
+                    if not self.monitoring:
+                        logger.info("<%s> not monitored anymore" % str(self))
+                        return False
+                    self.robot.wait_for_state_update(ACTIVE_SLEEP_RESOLUTION)
 
         else:
             #dummy mode. Wait a little bit, and assume the condition is true

commit 59cf8c1de155a0ebe103b0ab32e00f8294c70949

Fixed bug introduced in 0745cb3cdd in events.py
Hello Severin ;-)

src/robots/events.py
@@ -114,8 +114,8 @@ class EventMonitor:
 
         # store initial value, used by INCREASE/DECREASE modes
         # and last value, used by BECOMES modes
-        if not self.robot.dummy:
-            self.start_inc_value = self.robot.state[self.var] 
+        if (not self.robot.dummy) and (not callable(self.var)):
+            self.start_inc_value = self.robot.state[self.var]
             self.start_dec_value = self.robot.state[self.var] 
             self.last_value = self.robot.state[self.var] 
 
@@ -140,6 +140,10 @@ class EventMonitor:
                 self.target = decrease
             else:
                 raise Exception("Event created without condition!")
+        else:
+            self.mode = ""
+            self.target= ""
+
 
         logger.info("Added new event monitor: %s" % self)
 
@@ -176,8 +180,9 @@ class EventMonitor:
 
                     # after a blocking event, reset the reference values for
                     # events INCREASE and DECREASE
-                    self.start_inc_value = self.robot.state[self.var] 
-                    self.start_dec_value = self.robot.state[self.var] 
+                    if not callable(self.var):
+                        self.start_inc_value = self.robot.state[self.var]
+                        self.start_dec_value = self.robot.state[self.var]
 
             if self.oneshot:
                 logger.info("Removing event on %s" % self)
@@ -197,6 +202,8 @@ class EventMonitor:
     def _check_condition(self, val):
 
         ok = False
+        self.start_dec_value = 0
+        self.start_inc_value = 0
 
         if self.mode == EventMonitor.VALUE and val == self.target:
             ok = True
@@ -224,11 +231,14 @@ class EventMonitor:
 
 
     def _wait_for_condition(self):
-
+        import time
         if not self.robot.dummy:
 
             # predicate-based event
             if callable(self.var):
+                if not self.monitoring:
+                    logger.info("<%s> not monitored anymore" % str(self))
+                    return False
                 while not self.var(self.robot):
                     time.sleep(ACTIVE_SLEEP_RESOLUTION)
 
@@ -248,7 +258,7 @@ class EventMonitor:
 
         else:
             #dummy mode. Wait a little bit, and assume the condition is true
-            import time
+
             time.sleep(0.2)
         logger.info("%s is true" % str(self) + (" (dummy mode)" if self.robot.dummy else ""))
         return True